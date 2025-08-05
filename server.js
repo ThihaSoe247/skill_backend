5751a542f095597ce9c2cdf521aa6afce0f5e0ed
Thiha Soe <thihasos14122001@gmail.com>
2025-08-05 14:56:26 +0700

    cors added

--- server.js
+++ server.js
@@ -8,23 +8,8 @@ const skillRoutes = require("./routes/skills");
 dotenv.config();
 const app = express();
 app.use(express.json());
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://skill-frontend-six.vercel.app",
-];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*", credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("SkillSwap API is running");
